d2b059c50f3c8ac80b4168f19b71a0390863600e
Yuvraj Singh <113302796+Yuvrajsinghspd09@users.noreply.github.com>
2024-08-28 00:15:54 +0530

    Create GPT2FeatureExtractor.py
    Step-1

---
+++ GPT2FeatureExtractor.py
@@ -0,0 +1,24 @@
+# STEP_1  - Setting up the Env
+pip install torch transformer pyyaml
+
+import torch
+import torch.nn as nn
+from transformers load GPT2Toeknizer, GPT2Model
+from torch.utils.data import DataLoader, TensorDataset
+
+
+# STEP_2 GPT2FeatureExtractor
+
+class GPT2FeatureExtractor:
+  def __init__(self,model_name='gpt2'):
+    self.tokenizer=GPT2Tokenizer.from_pretrained(model_name)
+    self.model= GPT2Model.from_pretrained(model_name)
+
+  def extract_features(self,text):
+    inputs = self.tokenizer(text,return_tensors='pt',padding=True,truncation=True)
+    with torch.no_grad():
+      output= self.model(**inputs)
+      return outputs.last_hidden_state.mean(dim=1)
+
+
+
